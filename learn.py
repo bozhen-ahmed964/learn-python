717a0eaa705e2c713298fc96c434a012c2f2ed7a
bozhen964 <bozhencomputer964@gmail.com>
2022-10-16 22:57:04 +0300

    ezy task

--- learn.py
+++ learn.py
@@ -1,12 +1,38 @@
-#Many Values to Multiple Variables
-x, y, z = "Orange", "Banana", "Cherry"
-print(x)
-print(y)
-print(z)
+# lenght =  22.4
+# width = 50.9
+# area = lenght * width
+# print(int(area))
 
-#One Value to Multiple Variables
+#================================================================
 
-x = y = z = "Orange"
-print(x)
-print(y)
-print(z)
+# name = "bozhen"
+# age = "22"
+# address = "qaradax"
+# email = "bozhen964@gmail.com"
+# print(name, age, address, email)
+
+#================================================================
+
+
+# name = input("Name : ")
+# age = input("Age : ")
+# address = input("Address : ")
+# email = input("Email : ")
+# print(name, age, address, email)
+
+#================================================================
+
+# d1 = 14.8
+# d2 = 5.3
+
+# add = d1 + d2
+# sub = d1 - d2
+# mul = d1 * d2
+# div = d1 / d2
+
+# print(int(add))
+# print(int(sub))
+# print(int(mul))
+# print(int(div))
+
+#================================================================
